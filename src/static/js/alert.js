72a48cb04ae871f59f4eadda222eaa8db0702517
Juan-Carlos-Guevara <49172854+Juan-Carlos-Guevara@users.noreply.github.com>
2022-05-31 12:34:37 -0500

    Delete src directory

--- src/static/js/alert.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    setTimeout(function() {
-    $(".disappear").fadeOut(1500);
-    },4000);
-
-    setTimeout(function() {
-        $(".disappear_login").fadeIn(1500);
-    },2500);
-});
